4bd8848125c243f4fe4c4317fdfd3226658f3200
Enes Samet Aydı <esa.aydi@gmail.com>
2022-05-14 17:09:31 +0300

    form içerikleri maile bağlandı

--- Js/Main.js
+++ Js/Main.js
@@ -56,7 +56,7 @@ window.addEventListener('scroll', scrollActive)
 /*==================== Sitede Gezerken Menünün vurgulanması ==============*/
 function scrollHeader(){
     const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  
     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
